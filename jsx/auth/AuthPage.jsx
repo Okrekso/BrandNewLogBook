f86ba422c8574d2740cbd4727a9734be57f863e7
Okrekso <semikoseker@gmail.com>
2020-05-19 17:15:25 +0300

    Auth page upd - added material-ui to auth page - added error handler on auth - added localstorage saver for token & email

--- jsx/auth/AuthPage.jsx
+++ jsx/auth/AuthPage.jsx
@@ -1,15 +1,73 @@
 import React, { useState, useContext, useEffect, useCallback, useMemo } from 'react';
 import "./AuthPage.scss";
 import { CSSTransition } from "react-transition-group";
+import Auth from '../../js/auth';
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import ErrorIcon from '@material-ui/icons/Error';
 
 export default function AuthPage(props) {
+    const [email, setemail] = useState("");
+    const [password, setpassword] = useState("");
+    const [error, seterror] = useState("");
+    const [state, setstate] = useState("inputs");
+
+    useEffect(() => {
+        if (state == "log-in") {
+            Auth.signIn(email, password)
+                .then(res => {
+                    const authParams = { token: res.data.token, email: email };
+                    console.log(token);
+                    localStorage.setItem("authParams", authParams);
+                })
+                .catch(err => {
+                    setstate("inputs");
+                    seterror("Log In error. Check entered fields");
+                });
+        }
+    }, [state]);
+
+
     return (
         <CSSTransition classNames="flow-bottom" timeout={800} in appear>
             <div id="auth" className="full-page">
                 <div className="form">
-                    <input type="email" placeholder="email" />
-                    <input type="password" placeholder="password" />
-                    <button>Log In</button>
+                    {
+                        state == "inputs" &&
+                        <>
+                            <TextField
+                                label="email"
+                                type="email"
+                                className="text-fields"
+                                value={email}
+                                onChange={handle => setemail(handle.target.value)}
+                            />
+                            <TextField
+                                label="password"
+                                type="password"
+                                className="text-fields"
+                                value={password}
+                                onChange={handle => setpassword(handle.target.value)}
+                            />
+                            {
+                                error !== "" &&
+                                <div id="error-holder">
+                                    <ErrorIcon color="error" />
+                                    <p>{error}</p>
+                                </div>
+                            }
+                            <Button variant="contained" color="primary"
+                                onClick={() => setstate("log-in")}
+                                disabled={email.length < 5 || password.length < 2}>
+                                Log In
+                            </Button>
+                        </>
+                    }
+                    {
+                        state == "log-in" &&
+                        <CircularProgress id="circular" color="primary" />
+                    }
                 </div>
             </div>
         </CSSTransition>
--- jsx/render.jsx
+++ jsx/render.jsx
@@ -8,7 +8,21 @@ import {
 import AuthPage from './auth/AuthPage';
 import Header from './header/header';
 import AccountPage from './account/AccountPage';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from "@material-ui/core/styles"
+import blue from '@material-ui/core/colors/blue';
 
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#0066ff"
+        },
+        secondary: blue,
+        error: {
+            main: "#df3030"
+        }
+    },
+});
 
 export default function render(to) {
     ReactDOM.render(<Site />, to);
@@ -17,15 +31,17 @@ export default function render(to) {
 function Site(props) {
     return (
         <Router>
-            <Header />
-            <Switch>
-                <Route path="/" exact>
-                    <AuthPage />
-                </Route>
-                <Route path="/account">
-                    <AccountPage />
-                </Route>
-            </Switch>
+            <ThemeProvider theme={theme}>
+                <Header />
+                <Switch>
+                    <Route path="/" exact>
+                        <AuthPage />
+                    </Route>
+                    <Route path="/account">
+                        <AccountPage />
+                    </Route>
+                </Switch>
+            </ThemeProvider>
         </Router>
     );
 }
